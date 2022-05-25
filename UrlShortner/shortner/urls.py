a19c77f75a146b04a8f0ad96a4a312fdb2f27818
Shriyanshi Sharma <shriyanshi.19b131153@abes.ac.in>
2022-05-25 15:22:44 +0530

    UrlShortner

--- UrlShortner/shortner/urls.py
+++ UrlShortner/shortner/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('<str:pk>', views.go, name='go')
 ]
