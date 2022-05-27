d400e3a039ae3a84a81a253c403fc23b730dd89b
Shriyanshi Sharma <shriyanshi.19b131153@abes.ac.in>
2022-05-27 15:47:41 +0530

    Todo-List

--- Todo_List/Todo_List/settings.py
+++ Todo_List/Todo_List/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todolist',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Todo_List.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
